0c3c38340ea30d81367ba58802fd1088686760a6
Libertadoo <linda.vorobeja@inbox.lv>
2018-04-26 13:39:52 +0200

    READ MORE working, subpages kinda styled

--- script.js
+++ script.js
@@ -3,65 +3,76 @@ let events = document.querySelector("#events");
 let page = 1;
 let lookingForData = false;
 
-function fetchEvents(){
-  lookingForData=true;
+function fetchEvents() {
+	lookingForData = true;
 
-  let urlParams = new URLSearchParams(window.location.search);
+	let urlParams = new URLSearchParams(window.location.search);
 
-  let tagid = urlParams.get("tag");
-  let endpoint = "http://libertadoo.com/kea/second_semester/huset/wordpress/wp-json/wp/v2/event?_embed&per_page=2&page="+page
-  if(tagid){ // DRY
-    endpoint = "http://libertadoo.com/kea/second_semester/huset/wordpress/wp-json/wp/v2/event?_embed&per_page=2&page="+page + "&tags="+tagid
-  }
-    fetch(endpoint)
-      .then(e => e.json())
-      .then(showEvents);
-// http://libertadoo.com/kea/second_semester/huset/wordpress/wp-json/wp/v2/event
+	let tagid = urlParams.get("tag");
+	let endpoint = "http://libertadoo.com/kea/second_semester/huset/wordpress/wp-json/wp/v2/event?_embed&per_page=2&page=" + page
+	if (tagid) { // DRY
+		endpoint = "http://libertadoo.com/kea/second_semester/huset/wordpress/wp-json/wp/v2/event?_embed&per_page=2&page=" + page + "&tags=" + tagid
+	}
+	fetch(endpoint)
+		.then(e => e.json())
+		.then(showEvents);
+	// http://libertadoo.com/kea/second_semester/huset/wordpress/wp-json/wp/v2/event
 
 }
 
-function showEvents(data){
-  console.log(data)
-  lookingForData=false;
-  data.forEach(showSingleEvent);
+function showEvents(data) {
+	console.log(data)
+	lookingForData = false;
+	data.forEach(showSingleEvent);
 }
 
-function showSingleEvent(Event){
-  let clone = template.cloneNode(true);
-  clone.querySelector("h1").textContent = Event.title.rendered;
-  //clone.querySelector(".descript").innerHTML = Event.content.rendered;
-	clone.querySelector(".date").textContent = Event.acf.date;
+function showSingleEvent(Event) {
+	let clone = template.cloneNode(true);
+	clone.querySelector("h3").textContent = Event.title.rendered;
+	//clone.querySelector(".descript").innerHTML = Event.content.rendered;
+	clone.querySelector(".time").textContent = Event.acf.time;
+	//	clone.querySelector(".tagName").textContent = item.name;
+	// DATE
+
+let year = Event.acf.date.substring(0, 4);
+let month = Event.acf.date.substring(4, 6);
+let day = Event.acf.date.substring(6, 8);
+
+clone.querySelector(".date").textContent = day + "." + month + "." + year;
 
 
 
-  if(Event._embedded["wp:featuredmedia"]){//img is there
-     clone.querySelector("img").setAttribute("src", Event._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url)
-  } else { // no img
-      clone.querySelector("img").remove()
-  }
+	if (Event._embedded["wp:featuredmedia"]) { //img is there
+		clone.querySelector("img").setAttribute("src", Event._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url)
+	} else { // no img
+		clone.querySelector("img").remove()
+	}
 
-  clone.querySelector('.readmore').href="subpage.html?id=" + Event.id;
+	clone.querySelector('.readmore').href = "subpage.html?id=" + Event.id;
 
 
-  events.appendChild(clone);
+	events.appendChild(clone);
 }
 fetchEvents();
 
 
 //found this stuff online
-setInterval(function(){
+setInterval(function () {
 
-  if(bottomVisible() && lookingForData===false){
-    console.log("We've reached rock bottom, fetching articles")
-    page++;
-    fetchEvents();
-  }
+	if (bottomVisible() && lookingForData === false) {
+		console.log("We've reached rock bottom, fetching articles")
+		page++;
+		fetchEvents();
+	}
 }, 1000)
 
 function bottomVisible() {
-  const scrollY = window.scrollY
-  const visible = document.documentElement.clientHeight
-  const pageHeight = document.documentElement.scrollHeight
-  const bottomOfPage = visible + scrollY >= pageHeight
-  return bottomOfPage || pageHeight < visible
+	const scrollY = window.scrollY
+	const visible = document.documentElement.clientHeight
+	const pageHeight = document.documentElement.scrollHeight
+	const bottomOfPage = visible + scrollY >= pageHeight
+	return bottomOfPage || pageHeight < visible
 }
+
+
+
--- subpage.js
+++ subpage.js
@@ -4,16 +4,27 @@ let id = urlParams.get("id");
 console.log("i want to get article: " + id);
 
 
-fetch("http://libertadoo.com/kea/second_semester/huset/wordpress/wp-json/wp/v2/event"+id)
+fetch("http://libertadoo.com/kea/second_semester/huset/wordpress/wp-json/wp/v2/event/"+id)
   .then(e=>e.json())
   .then(showSinglePost)
 
 
-function showSinglePost(event){
-  console.log(event);
-  document.querySelector("#singleEvent h1").textContent=event.title.rendered;
+function showSinglePost(Event){
+  console.log(Event);
+  document.querySelector("#singleEvent h3").textContent=Event.title.rendered;
+document.querySelector(".desc").innerHTML = Event.content.rendered;
+document.querySelector(".time").innerHTML = Event.acf.time;
+document.querySelector(".price").innerHTML = Event.acf.price + " dkk";
+let year = Event.acf.date.substring(0, 4);
+let month = Event.acf.date.substring(4, 6);
+let day = Event.acf.date.substring(6, 8);
 
+document.querySelector(".date").innerHTML = day + "." + month + "." + year;
 
-  //show carsection
+  //show events
   document.querySelector("#singleEvent").classList.add("slideInEvent");
 }
+
+
+
+
